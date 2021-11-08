cc7bb67ed835b2f70148a960f0699520c656661d
Haris-Ali <harisali72000@gmail.com>
2021-11-08 22:27:51 +0500

    first route added

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const bcrypt = require('bcrypt')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("Authentication with node and express using bcrypt")
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
