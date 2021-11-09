1f0f668b2de03f4ec13d1b4f8c48648507a88c7d
Haris Ali Akhter <haris.ali@vizteck.com>
2021-11-09 14:35:08 +0500

    working on signup password hasing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("Authentication with node and express using bcrypt")
@@ -12,8 +13,18 @@ app.post('/login', (req, res) => {
 
 })
 
-app.post('/signup', (req, res) => {
-
+app.post('/signup', async (req, res) => {
+    try {
+        const username = req.body.username
+        const password = req.body.password
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+        console.log(username, password, salt, hashedPassword)
+        res.statusCode(200)
+    } catch {
+        console.error("Error")
+        res.statusCode(500)
+    }
 })
 
 app.listen(PORT, () => {
